47de3807c711e71f8e5553a398947ccb54f0ec43
Jeod <47716344+JeodC@users.noreply.github.com>
2024-12-28 17:26:44 -0500

    Initial commit

---
+++ Ori and the Blind Forest.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR=/$directory/ports/ori
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Display loading splash
+[ "$CFW_NAME" == "muOS" ] && $ESUDO $GAMEDIR/splash "$SPLASH" 1
+$ESUDO $GAMEDIR/splash "$SPLASH" 30000 & 
+
+# Exports
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export WINEPREFIX=~/.wine64
+export WINEDEBUG=-all
+
+# Install dependencies
+if ! winetricks list-installed | grep -q "^dxvk$"; then
+    pm_message "Installing dependencies."
+    winetricks dxvk
+fi
+
+# Determine exe and setup config folders
+mkdir -p $GAMEDIR/config
+if [ -f "$GAMEDIR/data/oriDE.exe" ]; then
+    EXE="oriDE.exe"
+    SPLASH="splashDE.png"
+    bind_directories "$WINEPREFIX/drive_c/users/root/AppData/Local/Ori and the Blind Forest DE" "$GAMEDIR/config"
+else
+    EXE="ori.exe"
+    SPLASH="splash.png"
+    bind_directories "$WINEPREFIX/drive_c/users/root/AppData/Local/Ori and the Blind Forest" "$GAMEDIR/config"
+fi
+
+# Run the game
+$GPTOKEYB "$EXE" -c "./ori.gptk" &
+box64 wine64 "./data/$EXE"
+
+# Kill processes
+wineserver -k
+pm_finish
---
+++ README.md
@@ -0,0 +1,7 @@
+# PortMaster Wine - Ori and the Blind Forest
+This isn't really a port, it's Ori and the Blind Forest running on wine64 and box64. It will boot and play on the Retroid Pocket 5 and Retroid Pocket Mini with a Rocknix distribution that has box64 and wine64 bundled. Such distributions (dev builds) can be found on the Rocknix Discord server.
+
+## Installation
+Copy the `ori` folder and `Ori and the Blind Forest.sh` file to the same place you would normally place your port files e.g. `/roms/ports`. This port uses the Steam version with Wine64. Purchase and install the game with Steam on Windows (or download the Windows depot) and copy all of its data to `ori/data`. You can use the Definitive Edition data or the original game data. The launchscript should detect which version you're using and launch accordingly.
+
+If you want to set up the emulationstation decorations, copy `cover.png` to the `ori` folder and copy the contents of `gameinfo.xml` to your `gamelist.xml` file.
