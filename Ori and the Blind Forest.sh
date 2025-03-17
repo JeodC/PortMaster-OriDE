1c4f529b54819deb106931aca6cd05cd4f1edd75
Jeod <47716344+JeodC@users.noreply.github.com>
2025-03-17 16:47:28 -0400

    Update Ori and the Blind Forest.sh

--- Ori and the Blind Forest.sh
+++ Ori and the Blind Forest.sh
@@ -15,41 +15,48 @@ get_controls
 # Variables
 GAMEDIR="/$directory/windows/ori"
 
-# CD and set permissions
+# CD and set log
 cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-$ESUDO chmod +x -R $GAMEDIR/*
-
-# Display loading splash
-[ "$CFW_NAME" == "muOS" ] && $ESUDO $GAMEDIR/splash "$SPLASH" 1
-$ESUDO $GAMEDIR/splash "$SPLASH" 30000 & 
 
 # Exports
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export WINEPREFIX=~/.wine64
 export WINEDEBUG=-all
-
-# Install dependencies
-if ! winetricks list-installed | grep -q "^dxvk$"; then
-    pm_message "Installing dependencies."
-    winetricks dxvk
-fi
 
 # Determine exe and setup config folders
 mkdir -p $GAMEDIR/config
 if [ -f "$GAMEDIR/data/oriDE.exe" ]; then
-    EXE="oriDE.exe"
+    EXEC="oriDE.exe"
     SPLASH="splashDE.png"
     bind_directories "$WINEPREFIX/drive_c/users/root/AppData/Local/Ori and the Blind Forest DE" "$GAMEDIR/config"
 else
-    EXE="ori.exe"
+    EXEC="ori.exe"
     SPLASH="splash.png"
     bind_directories "$WINEPREFIX/drive_c/users/root/AppData/Local/Ori and the Blind Forest" "$GAMEDIR/config"
 fi
 
+# Determine architecture
+if file "$GAMEDIR/data/$EXEC" | grep -q "PE32" && ! file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEARCH=win32
+    export WINEPREFIX=~/.wine32
+elif file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEPREFIX=~/.wine64
+else
+    echo "Unknown file format"
+fi
+
+# Display loading splash
+$ESUDO $GAMEDIR/splash "$SPLASH" 30000 & 
+
+# Install dependencies
+if ! winetricks list-installed | grep -q "^dxvk2041$"; then
+    pm_message "Installing dependencies."
+    winetricks dxvk2041
+fi
+
 # Run the game
-$GPTOKEYB "$EXE" -c "./ori.gptk" &
-box64 wine64 "./data/$EXE"
+$GPTOKEYB "$EXEC" -c "$GAMEDIR/ori.gptk" &
+box64 wine "$GAMEDIR/data/$EXEC"
 
 # Kill processes
 wineserver -k
